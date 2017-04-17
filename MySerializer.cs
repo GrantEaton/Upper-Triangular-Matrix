d6db04256403ec574f7a3e40f4a394a16d2f7ff8
Grant Eaton <grantman100@gmail.com>
2017-04-17 15:34:06 -0400

    update to serializer

--- MySerializer.cs
+++ MySerializer.cs
@@ -5,34 +5,46 @@ namespace Serializer {
 	public class MySerializer {
 		public static string Serialize(Object obj) {
 			Type type = obj.GetType();
+			string serial = "";
 			foreach (FieldInfo info in type.GetFields()){
-				Console.WriteLine(info.Name + info.GetValue(obj));	
+				serial += (info.Name + " = " + info.GetValue(obj) +"; ");
 			}
-			return "hello world";
+			//Console.WriteLine(serial);
+			return serial;
 		}
 		public static T Deserialize<T>(string str) {
-			Type tyle = typeof(T);
+			IList<string> values  = str.Split(';').ToList<string>();
+			
+
+			Type type = typeof(T);
 			ConstructorInfo ctor = type.GetConstructor(new Type[] {});
-			T obj = (T_ctor.Invoke(new Object[] {});
-			return object;
+			T obj = (T)ctor.Invoke(new Object[] {});
+			return obj;
 		}
 	}
 	public class Point {
 		public int x, y;
+		public bool tf;
 		public Point() {
+			tf = false;
 			x = y = 0;
 		}
-		public Point(int X, int Y) {
+		public Point(int X, int Y, bool tf) {
 			x = X;
 			y = Y;
+			tf = false;
+		}
+		public void toString(){
+			Console.WriteLine("output: {0} {1} {2}", x,y,tf);	
 		}
 	}
 	public class Test {
 		public static void Main(String [] args) {	
-			Point p1 = new Point(2, 3);
+			Point p1 = new Point(2, 3, true);
 			String str1 = MySerializer.Serialize(p1);
 			Console.WriteLine(str1);
 			Point newPt = MySerializer.Deserialize<Point>(str1);
+			newPt.toString();
 			Console.WriteLine(newPt);
 		}
 	}
